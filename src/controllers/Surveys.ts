dc2e839c8ed43ed1bd9234f786f86fc36e66a8a1
Petterson Neves <pettersonkesler@gmail.com>
2022-12-15 21:16:33 -0300

    refactor: create new routes with mocks

--- src/controllers/Surveys.ts
+++ src/controllers/Surveys.ts
@@ -10,6 +10,171 @@ export class Surveys {
 	@Render("survey")
 	create(@Body() body: any) {
 		console.log(body);
-		return body;
+		const data = {
+			id: 1,
+			name: "Survey Title",
+			desc: "Survey Description",
+			options: [
+				{
+					id: 1,
+					value: "first option",
+					image: "/img.jpg",
+					answers: 2,
+				},
+				{
+					id: 1,
+					value: "second option",
+					image: "/img.jpg",
+					answers: 5,
+				},
+				{
+					id: 1,
+					value: "thirt option",
+					image: "/img.jpg",
+					answers: 1,
+				},
+			],
+			chooseMany: false,
+			createDate: "14/12/2022",
+			closeDate: "15/12/2022",
+		};
+		return data;
 	}
+
+	// @UseBefore(bodyParser.urlencoded())
+	@Get("/list")
+	@Render("survey")
+	list() {
+		const data = {
+			surveys: [
+				{
+					id: 1,
+					name: "Survey A",
+					desc: "Survey Description",
+					options: [
+						{
+							id: 1,
+							value: "first option",
+							image: "/img.jpg",
+							answers: 2,
+						},
+						{
+							id: 1,
+							value: "second option",
+							image: "/img.jpg",
+							answers: 5,
+						},
+						{
+							id: 1,
+							value: "thirt option",
+							image: "/img.jpg",
+							answers: 1,
+						},
+					],
+					chooseMany: false,
+					createDate: "14/12/2022",
+					closeDate: "15/12/2022",
+				},
+				{
+					id: 2,
+					name: "Survey B",
+					desc: "Survey Description",
+					options: [
+						{
+							id: 1,
+							value: "first option",
+							image: "/img.jpg",
+							answers: 2,
+						},
+						{
+							id: 1,
+							value: "second option",
+							image: "/img.jpg",
+							answers: 5,
+						},
+						{
+							id: 1,
+							value: "thirt option",
+							image: "/img.jpg",
+							answers: 1,
+						},
+					],
+					chooseMany: false,
+					createDate: "14/12/2022",
+					closeDate: "15/12/2022",
+				},
+			]
+		};
+		return data;
+	}
+
+	@Get("/answer")
+	@Render("survey-answer")
+	cont() {
+		const data = {
+			id: 1,
+			name: "Survey A",
+			desc: "Survey Description",
+			options: [
+				{
+					id: 1,
+					value: "first option",
+					image: "/img.jpg",
+					answers: 2,
+				},
+				{
+					id: 1,
+					value: "second option",
+					image: "/img.jpg",
+					answers: 5,
+				},
+				{
+					id: 1,
+					value: "thirt option",
+					image: "/img.jpg",
+					answers: 1,
+				},
+			],
+			chooseMany: false,
+			createDate: "14/12/2022",
+			closeDate: "15/12/2022",
+		};
+		return data;
+	}
+
+	@Get("/:id")
+	@Render("answer-count")
+	show() {
+		const data = {
+			id: 1,
+			name: "Survey A",
+			desc: "Survey Description",
+			options: [
+				{
+					id: 1,
+					value: "first option",
+					image: "/img.jpg",
+					answers: 2,
+				},
+				{
+					id: 1,
+					value: "second option",
+					image: "/img.jpg",
+					answers: 5,
+				},
+				{
+					id: 1,
+					value: "thirt option",
+					image: "/img.jpg",
+					answers: 1,
+				},
+			],
+			chooseMany: false,
+			createDate: "14/12/2022",
+			closeDate: "15/12/2022",
+		};
+		return data;
+	}
+
+	
 }
